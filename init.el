cbb3392991cf6019b348ee2ebf41dfee7ad2d5ee
Jason Darby <jasonkdarby@gmail.com>
2022-03-14 00:54:16 -0400

    use straight.el, it should be fast right?

--- init.el
+++ init.el
@@ -2,18 +2,24 @@
 
 ;; BEGIN emacs things not related to specific packages
 
-(require 'package)
+(setq package-enable-at-startup nil)
 
-(setq package-archives
-      '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
-        ("MELPA Stable" . "https://stable.melpa.org/packages/")
-        ("MELPA"        . "https://melpa.org/packages/"))
-      package-archive-priorities
-      '(("MELPA Stable" . 10)
-        ("GNU ELPA"     . 5)
-        ("MELPA"        . 0)))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(package-initialize)
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
 
 ;; Delete by moving to trash instead of just poof
 (setq-default delete-by-moving-to-trash t)
